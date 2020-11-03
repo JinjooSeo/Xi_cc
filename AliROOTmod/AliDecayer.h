18d4fed30491cf2dbbea87a291d14567eb233a13
Jinjoo Seo <jinjoo@Jinjoos-MacBook-Pro.local>
2020-11-03 14:15:39 +0900

    AliRoot/EVGEN/AliDecayer.h AliRoot/PYTHIA8/AliPYTHIA8/Ali~

---
+++ AliROOTmod/AliDecayer.h
@@ -0,0 +1,32 @@
+#ifndef ALI_DECAYER__H
+#define ALI_DECAYER__H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+#include "RVersion.h"
+#include "TVirtualMCDecayer.h"
+
+typedef TVirtualMCDecayer AliDecayer;
+
+#if ROOT_VERSION_CODE >= 197633  //Corresponds to Root v3-04-01
+typedef enum 
+{
+  kBSemiElectronic, kSemiElectronic, kDiElectron, kBSemiMuonic, kDSemiMuonic, kSemiMuonic, kDiMuon, kJpsiDiMuon,
+    kBJpsiDiMuon, kBJpsiDiElectron, 
+    kBPsiPrimeDiMuon, kBPsiPrimeDiElectron, kPiToMu, kKaToMu, 
+    kNoDecay, kHadronicD, kHadronicDWithout4Bodies, kOmega, kLambda, kPhiKK, 
+    kAll, kNoDecayHeavy, kHardMuons, kBJpsi,  kBJpsiUndecayed,
+    kWToMuon,kWToCharm, kWToCharmToMuon, kZDiMuon, kZDiElectron, kNeutralPion, kAllMuonic,
+    kChiToJpsiGammaToMuonMuon, kChiToJpsiGammaToElectronElectron, kNoDecayBeauty, kPsiPrimeJpsiDiElectron,
+    kElectronEM, kGammaEM, kDiElectronEM, kBeautyUpgrade,
+    kHadronicDWithV0,kHadronicDWithout4BodiesWithV0,kAllEM,
+    kLcpKpi, kLcpK0S, kHFYellowReport, kHadronicDPionicD0, kHadronicDWithV0PionicD0, kHadronicDWithout4BodiesPionicD0, 
+    kHadronicDWithout4BodiesWithV0PionicD0,kHadronicDPionicD0pure,kHadronicDPionicD0K,kHadronicDPionicD0pi, kLcpK0SBDTsig, kEtaPrime,
+    kXic0Semileptonic,kHadronicDWithout4BodiesDsPhiPi,kHFLS4Upgrade
+} Decay_t;
+#endif
+
+#endif //ALI_DECAYER__H
+
---
+++ AliROOTmod/AliDecayerPythia8.h
@@ -0,0 +1,61 @@
+#ifndef ALIDECAYERPYTHIA8_H
+#define ALIDECAYERPYTHIA8_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$*/
+
+// Implementation of TVirtualMCDecayer using Pythia8
+// Author: andreas.morsch@cern.ch
+
+#include <ParticleData.h>
+#include <TVirtualMCDecayer.h>
+#include <AliDecayer.h>
+
+class AliDecayerPythia8;
+
+
+class AliDecayerPythia8 : public TVirtualMCDecayer {
+ public:
+  AliDecayerPythia8();
+  virtual ~AliDecayerPythia8(){;}
+  virtual void    Init();
+  virtual void    Decay(Int_t pdg, TLorentzVector* p);
+  virtual Int_t   ImportParticles(TClonesArray *particles);
+  virtual void    SetForceDecay(Decay_t decay) {fDecay=decay;}
+  virtual void    SetForceDecay(Int_t decay)   {SetForceDecay((Decay_t) decay);}
+  virtual void    ForceDecay();
+  virtual Float_t GetPartialBranchingRatio(Int_t ipart);
+  virtual void    HeavyFlavourOff() {fHeavyFlavour = kFALSE;}
+  virtual Float_t GetLifetime(Int_t kf);
+  virtual void    ReadDecayTable();
+
+		virtual void    ForceMultiHFLS4Upgrade();
+
+  virtual void    SetDebugLevel(Int_t debug) {fDebug = debug;}
+
+ protected:
+   void AppendParticle(Int_t pdg, TLorentzVector* p);
+   void ClearEvent(); 
+ private:
+  AliDecayerPythia8(const AliDecayerPythia8&);
+  AliDecayerPythia8 operator=(const AliDecayerPythia8&);
+  void     SwitchOffHeavyFlavour();
+  void     ForceHadronicD(Int_t optUser4Bodies = 1,Int_t optUseDtoV0 =1, Int_t optForceLcChannel = 0, Int_t optForceXicChannel = 0, Int_t optForceDsChannel = 0);
+  void  ForceBeautyUpgrade();
+  AliTPythia8*  fPythia8;          // Pointer to pythia8
+  Int_t         fDebug;            // Debug level
+ 
+  Decay_t       fDecay;            //  Forced decay mode
+  Bool_t        fHeavyFlavour;     //! Flag for heavy flavors
+  static Bool_t fgInit;            //! initialization flag 
+  ClassDef(AliDecayerPythia8, 2) // Particle Decayer using Pythia8
+};
+#endif
+
+
+
+
+
+
+
